b6af230d9b25e1e7acaf13b76f083f72ae3a9b4e
张牡丹 <1050339760@qq.com>
2020-05-14 21:53:59 +0800

    分类

--- pages/category/index.js
+++ pages/category/index.js
@@ -5,62 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    cateData: [],
+    items: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad(options) {
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/categories',
+      method: 'GET',
+      success: (result) => {
+        // console.log(result);
+        const cateData = result.data.message
+        this.setData({
+          cateData,
+          items: cateData[0]
+        })
+      },
+    });
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  navtap(e) {
+    const index = e.currentTarget.dataset.index;
+    this.setData({
+      items: this.data.cateData[index]
+    })
 
   }
+
+
 })
