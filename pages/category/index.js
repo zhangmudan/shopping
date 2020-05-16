d7bae31bf42c3171e1805ca4eee4cbe86e8d7f1e
张牡丹 <1050339760@qq.com>
2020-05-16 21:44:24 +0800

    分类页

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,52 +1,76 @@
 import axios from '../../request/myAxios'
 // pages/category/index.js
 Page({
+  // 页面的初始数据
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     //左侧
     cateData: [],
     activeIndex: 0,
     //右侧
-    cateRight: []
+    cateRight: [],
+    //右侧滚动归零
+    scropTop: 0
   },
   cateAll: [],
+  //请求数据
   getCateData() {
     axios({
       url: '/categories',
     }).then(res => {
       this.cateAll = res
       // console.log(this.cateAll);
-      const cateData = this.cateAll.map(item => {
-        return {
-          cat_id: item.cat_id,
-          cat_name: item.cat_name
-        }
-      })
-      const cateRight = this.cateAll[0].children
-
-      this.setData({
-        cateData,
-        cateRight,
-      })
+      this.getMap()
+      //数据获取成功后,把数据保存到本地存储
+      //因为本地存储属于永久存储,保存是添加一个时间戳
+      //时间戳用于对比下次打开当前页面的时间,如果超过某个时间范围,那么重新请求
+      wx.setStorageSync('cateAll', {
+        time: Date.now(),
+        data: this.cateAll
+      });
+    })
+  },
+  //处理数据
+  getMap() {
+    const cateData = this.cateAll.map(item => {
+      return {
+        cat_id: item.cat_id,
+        cat_name: item.cat_name
+      }
+    })
+    const cateRight = this.cateAll[0].children
+    //更新页面数据
+    this.setData({
+      cateData,
+      cateRight,
     })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+
+  // 生命周期函数--监听页面加载   
   onLoad(options) {
-    this.getCateData()
+    //获取本地存储数据
+    const cateAll = wx.getStorageSync('cateAll');
+    if (!cateAll) {
+      this.getCateData()
+    } else if (Date.now() - cateAll.time > 5 * 60 * 1000) {
+      // console.log('超过5分钟');
+      this.getCateData()
+    } else {
+      // console.log('未超过5分钟');
+      this.cateAll = cateAll.data
+      this.getMap()
 
-
+    }
   },
+  // 点击获取数据
   navtap(e) {
     const index = e.currentTarget.dataset.index;
     this.setData({
       activeIndex: index,
-      cateRight: this.cateAll[index].children
+      cateRight: this.cateAll[index].children,
+      //右侧滚动归零
+      scropTop: 0
     })
 
   }
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,32 @@
-// pages/goods_list/index.js
+import axios from '../../request/myAxios'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    nav: ["综合", "销量", "价格"],
+    navindex: 0,
+    goodsList: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    axios({ url: '/goods/search', data: { cid: 5 } }).then(res => {
+      // console.log(res);
+      const goodsList = res
+      this.setData({
+        goodsList
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击获取数据
+  navIndex(e) {
+    const index = e.currentTarget.dataset.nav;
+    this.setData({
+      navindex: index
+    })
   }
 })
