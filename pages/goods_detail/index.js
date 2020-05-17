5e08ead6c708e90af82237a96e2fc0f53b22c2c3
张牡丹 <1050339760@qq.com>
2020-05-17 22:04:20 +0800

    商品详情

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,39 @@
-// pages/goods_detail/index.js
+import axios from '../../request/myAxios'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsList: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    const goods_id = options.goods_id
+    axios({
+      url: '/goods/detail',
+      data: { goods_id }
+    }).then(res => {
+      // console.log(res);
+      const goodsList = res
+      this.setData({
+        goodsList
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  bigImage(e) {
+    const urls = this.data.goodsList.pics.map(item => {
+      return item.pics_big
+    })
+    const current = e.currentTarget.dataset.image
+    wx.previewImage({
+      urls,
+      current
+    })
   }
+
+
 })
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -6,28 +6,40 @@ Page({
    */
   data: {
     nav: ["综合", "销量", "价格"],
-    navindex: 0,
-    goodsList: {}
+    navindex: 0,//tab栏切换
+    goodsList: [],//商品数据
+
   },
+
+  //请求参数
   params: {
-    query: '',
-    cid: '',
-    pagenum: 1,
-    pagesize: 20
+    query: '',//关键字
+    cid: '',//商品id
+    pagenum: 1,//页码
+    pagesize: 10//页容量
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+
+  totalCount: 0,//总条数
+
+  //请求数据
+  getGoodsList() {
+    return axios({ url: '/goods/search', data: this.params }).then(res => {
+      // console.log(res);
+      //总条数
+      this.totalCount = res.total
+      //页面数据
+      this.setData({
+        //分页时数据连接 把原本的数据解构,把返回的数据也解构,拼接成一个数据
+        goodsList: [...this.data.goodsList, ...res.goods]
+      })
+
+    })
+  },
+  //  生命周期函数--监听页面加载
+  onLoad(options) {
     this.params.cid = options.cid || ""
     this.params.query = options.query || ""
-    axios({ url: '/goods/search', data: this.params }).then(res => {
-      // console.log(res);
-      const goodsList = res
-      this.setData({
-        goodsList
-      })
-    })
+    this.getGoodsList()
   },
   // 点击获取数据
   navIndex(e) {
@@ -35,5 +47,33 @@ Page({
     this.setData({
       navindex: index
     })
+  },
+  //触底刷新请求数据
+  onReachBottom() {
+    //Math.ceil(总条数/页容量) 计算页码 
+    if (Math.ceil(this.totalCount / this.params.pagesize) > this.params.pagenum) {
+      //页面累加
+      this.params.pagenum++;
+      this.getGoodsList()
+    } else {
+      wx.showToast({ title: '我也是有底线的', icon: 'none' });
+
+    }
+
+  },
+  //页面下拉刷新事件
+  async onPullDownRefresh() {
+    //页面数据
+    this.setData({
+      //清空数据
+      goodsList: []
+    })
+    //回到第一页
+    this.params.pagenum = 1;
+    //重新请求数据
+    // 异步变同步
+    await this.getGoodsList()
+    //停止上拉刷新
+    wx.stopPullDownRefresh()
   }
 })
