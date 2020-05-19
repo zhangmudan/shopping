dc9430593b39c55ff43cd5e334198224f0cc9bea
张牡丹 <1050339760@qq.com>
2020-05-19 12:05:26 +0800

    添加按钮

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -12,13 +12,20 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+
     const goods_id = options.goods_id
     axios({
       url: '/goods/detail',
       data: { goods_id }
     }).then(res => {
       // console.log(res);
-      const goodsList = res
+      let goodsList = res
+      //获取系统信息
+      const { system } = wx.getSystemInfoSync()
+      if (system.toLocaleLowerCase().indexOf('ios') > -1) {
+        //ios 平台把html 字符串中的 .webp 图片替换成 .jpg
+        goodsList.goods_introduce = goodsList.goods_introduce.replace(/.webp/g, '.jpg')
+      }
       this.setData({
         goodsList
       })
